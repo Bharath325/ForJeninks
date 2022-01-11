eef64f3515c22796fa8b8720adef89474c54a57b
002KBG744 <002KBG744@LAPTOP-I4NIJFCH>
2022-01-11 16:57:54 +0530

    first

---
+++ SeleniumMavenProj2/src/test/java/mavenFirst/VerifyGoogleTest.java
@@ -0,0 +1,40 @@
+package mavenFirst;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class VerifyGoogleTest {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setbrowser() {
+
+		System.setProperty("webdriver.edge.driver", "C:\\testingSoftware\\edgedriver_win32\\msedgedriver.exe");
+		driver = new EdgeDriver();
+		driver.get("http://google.com/");
+		driver.manage().window().maximize();
+		System.out.println("Opening browser");
+	}
+
+	@Test
+	public void checkTitle() {
+		System.out.println("checking title");
+		Assert.assertEquals(driver.getTitle(), "Google");
+	}
+
+	@Test(priority = 1)
+	public void checklogo() {
+		System.out.println("checking logo");
+		Assert.assertTrue(driver.findElement(By.xpath("//img[@alt='Google']")).isDisplayed());
+	}
+
+	@AfterMethod
+	public void closeWindow() {
+		driver.quit();
+	}
+}
