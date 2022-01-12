f005ce39401df2e6ab57aece9e7dafdb42595fa1
002KBG744 <002KBG744@LAPTOP-I4NIJFCH>
2022-01-12 16:51:16 +0530

    Tried fixing with properties

--- SeleniumMavenProj2/src/test/java/mavenFirst/VerifyGoogleTest.java
+++ SeleniumMavenProj2/src/test/java/mavenFirst/VerifyGoogleTest.java
@@ -1,5 +1,10 @@
 package mavenFirst;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -10,15 +15,35 @@ import org.testng.annotations.Test;
 
 public class VerifyGoogleTest {
 	public static void main(String args[]) {
+		Properties prop = new Properties();
+		InputStream input = null;
 
+		try {
+
+			input = new FileInputStream("config.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+			System.out.println(prop.getProperty("EDGE_HOME"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			System.out.println("In finally");
+		}
 	}
 
 	WebDriver driver;
 
 	@BeforeMethod
 	public void setbrowser() {
-
-		System.setProperty("webdriver.edge.driver", "C:\testingSoftware\\edgedriver_win32\\msedgedriver.exe");
+//		Properties prop = new Properties();
+//		InputStream input = null;
+//		input = new FileInputStream("config.properties");
+//		prop.load(input);
+//		System.setProperty("webdriver.edge.driver", prop.getProperty("EDGE_HOME"));
+		System.setProperty("webdriver.edge.driver", "C:\\testingSoftware\\edgedriver_win32\\msedgedriver.exe");
 		driver = new EdgeDriver();
 		driver.get("http://google.com/");
 		driver.manage().window().maximize();
